30ad58879dc0e1a24bea97e14c4bfe63fccd972c
ranjhal23 <ranjhal2003@gmail.com>
2024-05-23 09:00:35 +0530

    config

--- config/database.js
+++ config/database.js
@@ -2,7 +2,10 @@ const mongoose= require('mongoose')
 require("dotenv").config()
 
 exports.connect= ()=>{
-    mongoose.connect(process.env.MONGODB_URL)
+    mongoose.connect(process.env.MONGODB_URL,{
+        useNewURLParser: true,
+        useUnifiedTopology: true
+    })
     .then(()=>{console.log("DB successfull")})
     .catch((e)=>{
         console.log("Error in DB")
