ad909e01371e7ea06c07a28df620a522776f96e4
ranjhal23 <ranjhal2003@gmail.com>
2024-05-27 23:40:32 +0530

    new changes

---
+++ controllers/auth.js
@@ -0,0 +1,4 @@
+const bcrypt= require('bcrypt')
+const user= require('../models/User')
+
+//signup route handler
--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const PORT= process.env.PORT || 4000
 app.use(express.json())
 
 require("./config/database").connect() 
+
+const user= require("./routes/user")
+app.use('/api/v1', user)
+
+app.listen(PORT, ()=>{
+    console.log(`app is listened on${PORT} port`)
+})
