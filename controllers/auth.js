d5e20755c65c62e25dfc975762ad964cf71d08ce
ranjhal23 <ranjhal2003@gmail.com>
2024-06-03 16:32:13 +0530

    final

--- controllers/auth.js
+++ controllers/auth.js
@@ -78,7 +78,7 @@ exports.login= async (req, res)=>{
             user.token= token
             user.password= undefined
             const options={
-                expires: new Date(Date.now() + 30000),
+                expires: new Date(Date.now() + 3*24*60*60*1000),
                 httpOnly: true,
 
             }
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express= require('express')
 const router= express.Router()
+const User= require('../models/User')
 
 const{signup, login}= require('../controllers/auth')
 const{auth, isStudent, isAdmin}= require('../middlewares/auth')
@@ -29,6 +30,31 @@ router.get('/admin', auth, isAdmin, (req, res)=>{
        message: 'welcome to protected route for admin',
        
     })
+})
+router.get('/getEmail', auth, async(req, res)=> {
+   try{
+      const id= req.user.id;
+      const user= await User.findById(id)
+      res.status(200).json({
+         success: true,
+         user: user,
+         message: 'welcome to email route',
+         
+      })
+
+   }catch(e){
+      res.status(500).json({
+         success: false,
+         error: e.message,
+         message: 'fatt gaya',
+         
+      })
+
+   }
+
+   
+   console.log('ID:', id)
+   
 })
 
 module.exports=router
